61ec52f8996e7e3b7c810173f2b23404027b4362
Firemark <marpiechula@gmail.com>
2019-11-23 16:47:11 +0100

    add player object

--- src/game/game.py
+++ src/game/game.py
@@ -2,6 +2,19 @@
 import pyxel
 
 
+class Player:
+    WIDTH_SPRITE = 32
+    HEIGHT_SPRITE = 32
+
+    def __init__(self, cords):
+        self.score = 0
+        self.cords = cords
+
+    def draw(self):
+        x, y = self.cords
+        pyxel.blt(x, y, 0, 0, 0, self.WIDTH_SPRITE, self.HEIGHT_SPRITE)
+
+
 class App:
 
     def run(self):
@@ -11,13 +24,13 @@ class App:
         pyxel.init(256, 256, title="SPACESHIP OMG!!!")
         pyxel.image(0).load(0, 0, "assets.png")
 
+        self.player = Player([128, 128])
+
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
     def draw(self):
         pyxel.cls(0)
-        #pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
-        pyxel.blt(64, 64, 0, 0, 0, 32, 32)
-        pyxel.blt(64, 128, 0, 32, 32, 32, 32)
-        pyxel.blt(128, 128, 0, 64, 64, 32, 32)
+        pyxel.text(0, 0, "Score: %d" % self.player.score, 7)
+        self.player.draw()

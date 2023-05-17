f3b63a0855a8e2e473510ee30be1bb267eaec952
Firemark <marpiechula@gmail.com>
2023-05-17 22:55:59 +0200

    update prelection.sh script

--- generate_stages_for_readme.sh
+++ generate_stages_for_readme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 START=6000aff4aea00a51111c17b90ecd84a391432339
-END=c42ec86d9b632452fa6e1905965546f781632ec4
+END=d09df36ce9eb72b5edcc9ef0ec7f682c0d30f46f
 COMMITS=($START $(git log --pretty=tformat:%H $START..$END | tac))
 
 show_commit() {
--- prelection.sh
+++ prelection.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
+set -e
+trap "git checkout master --force" EXIT
 START=6000aff4aea00a51111c17b90ecd84a391432339
-END=c42ec86d9b632452fa6e1905965546f781632ec4
+END=d09df36ce9eb72b5edcc9ef0ec7f682c0d30f46f
 COMMITS=($START $(git log --pretty=tformat:%H $START..$END | tac))
 
 show_commit() {
-    #firefox https://github.com/firemark/pyxel-prelection/commit/$2 2> /dev/null
-
     LINE=$(python3 -c "print('#'*100)")
     FORMAT="format:%n$(expr $1 + 1). %s%n$LINE%n%n"
-    git show $2 --pretty="$FORMAT" | delta --dark -w 180 | less -RS
+    git show $2 --pretty="$FORMAT" | delta --line-numbers --true-color never --dark --paging=always
 }
 
 LEN="${#COMMITS[@]}"
@@ -16,7 +16,7 @@ for ((i=0;i<LEN;i++)); do
     COMMIT=${COMMITS[$i]}
     git checkout $COMMIT 2> /dev/null
     show_commit $i $COMMIT
+    set +e
     pyxel-prelection-game
+    set -e
 done
-
-git checkout master
